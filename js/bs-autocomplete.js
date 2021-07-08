b20159871f9dd6982cc022813a13030a9d624bed
Devi Tse <devitcf@gmail.com>
2021-07-08 10:19:30 +0800

    Add custom class support, remove unnecessary attributes, press esc to hide list

--- js/bs-autocomplete.js
+++ js/bs-autocomplete.js
@@ -4,7 +4,7 @@ Vue.component('bs-autocomplete', {
 		name: 'bs-autocomplete',
 		template: `
             <div style="position: relative" ref="outer_div">
-                <select value="value" :id="id" class="custom-select"  @click.stop.prevent="toggle_list" v-bind="$attrs" @keydown.38="press_up" @keydown.40="press_down" @keypress.enter.stop.prevent="press_enter">
+                <select :class="select_class" @click.stop.prevent="toggle_list" v-bind="$attrs" @keydown.38="press_up" @keydown.40="press_down" @keypress.enter.stop.prevent="press_enter">
                 	<option style="display:none" v-if="selected_item" selected>
                 		<slot name="display_text" v-bind="{item:selected_item}">{{ get_item_text(selected_item) }}</slot>
                 	</option>
@@ -12,7 +12,7 @@ Vue.component('bs-autocomplete', {
 	            <div class="bs-autocomplete" v-if="show">
 	            	<div class="bs-autocomplete-items">
 	            		<div class="p-2">
-	            			<input type="text" class="form-control" v-model="search_value" ref="search_input" :placeholder="placeholder" @keydown.38="press_up" @keydown.40="press_down" @keypress.enter.stop.prevent="press_enter" @input="active_index = null">
+	            			<input type="text" :class="input_class" v-model="search_value" ref="search_input" :placeholder="placeholder" @keyup.27="toggle_list('hide')" @keydown.38="press_up" @keydown.40="press_down" @keypress.enter.stop.prevent="press_enter" @input="active_index = null">
 						</div>
 			            <div class="list-group list-group-flush bs-autocomplete-list" :style="{'--bs-autocomplete-max-height': get_max_height()}">
 			                <a v-for="item, index in result_list" class="list-group-item list-group-item-action" :class="{'active': (index == active_index && !item.disable), 'disabled': (item.disable)}" style="cursor: pointer; padding-top: 5px; padding-bottom: 5px;" @click="select(item)">
@@ -25,7 +25,6 @@ Vue.component('bs-autocomplete', {
 `,
 		data() {
 			return {
-				id: this._uuid(),
 				selected_item: null,
 				search_value: '',
 				show: false,
@@ -64,6 +63,14 @@ Vue.component('bs-autocomplete', {
 		},
 		props: {
 			value: [String, Object],
+			select_class: {
+				type: String,
+				default: 'custom-select'
+			},
+			input_class: {
+				type: String,
+				default: 'form-control'
+			},
 			placeholder: {
 				type: String,
 				default: '',
@@ -99,23 +106,10 @@ Vue.component('bs-autocomplete', {
 						return true;
 					}
 					return this.item_labels.get(e).toLowerCase().includes(this.search_value.toLowerCase());
-
-					return false;
 				});
 			},
 		},
 		methods: {
-			_uuid() {
-				let d = Date.now();
-				if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
-					d += performance.now(); //use high-precision timer if available
-				}
-				return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-					var r = (d + Math.random() * 16) % 16 | 0;
-					d = Math.floor(d / 16);
-					return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-				});
-			},
 			sync_selected_item(value) {
 				this.selected_item = this.items.find(e => {
 					let item = this.return_attr == null ? e : e[this.return_attr];
@@ -149,27 +143,31 @@ Vue.component('bs-autocomplete', {
 				}
 			},
 			press_down(e) {
-				if (this.show) {
-					if (this.active_index == null) {
-						return this.active_index = 0;
-					}
-					if (this.active_index < this.result_list.length - 1) {
-						this.active_index++;
-					}
+				if (!this.show) {
+					this.toggle_list('show');
+				}
+				if (this.active_index == null) {
+					return this.active_index = 0;
+				}
+				if (this.active_index < this.result_list.length - 1) {
+					this.active_index++;
 				}
 			},
 			press_up(e) {
-				if (this.show) {
-					if (this.active_index == null) {
-						return this.active_index = 0;
-					}
-					if (this.active_index > 0) {
-						this.active_index--;
-					}
+				if (!this.show) {
+					this.toggle_list('show');
+				}
+				if (this.active_index == null) {
+					return this.active_index = 0;
+				}
+				if (this.active_index > 0) {
+					this.active_index--;
 				}
 			},
 			press_enter() {
-				if (this.show) {
+				if (!this.show) {
+					this.toggle_list('show');
+				} else {
 					return this.select(this.result_list[this.active_index]);
 				}
 			},
